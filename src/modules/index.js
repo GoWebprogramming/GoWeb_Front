947aebbbead851879e9ab752bbda3a346c4acd90
Jeongjin Oh <ojj991123@gmail.com>
2021-12-18 09:45:40 +0900

    Feat: main input 개발

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,15 +3,19 @@ import { all } from 'redux-saga/effects';
 import loading from './loading';
 import auth, { authSaga } from './auth';
 import user, { userSaga } from './user';
+import post, { postSaga } from './post';
+import reload, { reloadSaga } from './reload';
 
 const rootReducer = combineReducers({
   auth,
   loading,
   user,
+  post,
+  reload,
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), userSaga()]);
+  yield all([authSaga(), userSaga(), postSaga(), reloadSaga()]);
 }
 
 export default rootReducer;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
+import MainInputContainer from '../containers/common/MaininputContainer';
 
 // 사이트를 처음 방문 했을 때 보이는 view 입니다.
 const MainPage = () => {
   return (
     <>
       <HeaderContainer />
+      <MainInputContainer />
     </>
   );
 };
